bf8e7b843bcdcf2d21d7fd4c44ab50a80933f460
sinji2102 <sinzi031012@naver.com>
2022-10-04 18:52:25 +0900

    chore: analogWrite 수정

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW); 
+  delay(1000);
+  digitalWrite(7, HIGH);
+}
+
+void loop() {
+  int i;
+  i = 0;
+  while(i<5){
+    digitalWrite(7, LOW); 
+    delay(100);
+    digitalWrite(7, HIGH);
+    delay(100);
+    i = i + 1;
+  }
+  while(1){
+    
+  }
+}
