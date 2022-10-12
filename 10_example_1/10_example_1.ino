6469044d94730823d2b18d5e7cac91c6858d93d6
sinji2102 <sinzi031012@naver.com>
2022-10-12 09:16:26 +0900

    Add: 파일 여러개 추가

--- 10_example_1/10_example_1.ino
+++ 10_example_1/10_example_1.ino
@@ -4,11 +4,20 @@
 Servo myservo;
 
 void setup() {
-  myservo.attach(PIN_SERVO); 
-  myservo.write(90);
+  myservo.attach(PIN_SERVO);
+  myservo.write(90); 
   delay(1000);
+
 }
 
 void loop() {
     // add code here.
+  myservo.write(130);
+  delay(500);
+  myservo.write(90);
+  delay(500);
+  myservo.write(50);
+  delay(500);
+  myservo.write(90);
+  delay(500);
 }
